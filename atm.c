11e11a3bc87cb01853f7961ee09e01be7fff9c33
rajat <rajat4113@gmail.com>
2020-04-07 16:24:39 +0530

    refactored get_money function in atm.c

--- atm.c
+++ atm.c
@@ -1,4 +1,5 @@
 #include "atm.h"
+#include <math.h>
 
 unsigned int get_money(unsigned short int amount)
 {
@@ -7,13 +8,12 @@ unsigned int get_money(unsigned short int amount)
     return 0;
   }
   unsigned int denomination_count = 0x0;
-  unsigned short int notes[8] = {2000, 500, 100, 50, 20, 10, 5, 1};
+  unsigned short int notes[8] = {1, 5, 10, 20, 50, 100, 500, 2000};
   unsigned short int remaining_amount = amount;
 
-  for (int i = 0; i < 8; i++)
+  for (int i = 7; i >= 0; i--)
   {
-    denomination_count <<= 4;
-    denomination_count |= remaining_amount / notes[i];
+    denomination_count += pow(16, i) * (remaining_amount / notes[i]);
     remaining_amount %= notes[i];
   }
   return denomination_count;

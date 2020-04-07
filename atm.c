795b171596d8bbf4ebbfc0202eb8a830f38213cf
rajat <rajat4113@gmail.com>
2020-04-07 15:52:19 +0530

    modified get_money function now calculating 2000 rs note

--- atm.c
+++ atm.c
@@ -1,6 +1,9 @@
 #include "atm.h"
 
-unsigned int get_money(unsigned short int amount){
+unsigned int get_money(unsigned short int amount)
+{
   unsigned int denomination_count = 0x0;
+  denomination_count |= amount / 2000;
+  denomination_count <<= 28;
   return denomination_count;
 }

13aaf3823687c57d90627c0cf63d17ebe9f2755d
rajat <rajat4113@gmail.com>
2020-04-07 17:17:35 +0530

    added display_notes function in atm.c

--- atm.c
+++ atm.c
@@ -1,20 +1,33 @@
+#include <stdio.h>
 #include "atm.h"
-#include <math.h>
 
 unsigned int get_money(unsigned short int amount)
 {
-  if (amount > 31999)
-  {
-    return 0;
-  }
   unsigned int denomination_count = 0x0;
-  unsigned short int notes[8] = {1, 5, 10, 20, 50, 100, 500, 2000};
+  if (amount > 31999)
+    return denomination_count;
+  unsigned short int notes[8] = {2000, 500, 100, 50, 20, 10, 5, 1};
   unsigned short int remaining_amount = amount;
 
-  for (int i = 7; i >= 0; i--)
+  for (int i = 0; i < 8; i++)
   {
-    denomination_count += pow(16, i) * (remaining_amount / notes[i]);
+    denomination_count = denomination_count << 4;
+    denomination_count |= remaining_amount / notes[i];
     remaining_amount %= notes[i];
   }
   return denomination_count;
+}
+
+void display_notes(unsigned short int amount, unsigned int notes_count)
+{
+  unsigned short int notes[8] = {2000, 500, 100, 50, 20, 10, 5, 1};
+  printf("denomination count for %u\n", amount);
+  unsigned int count = 0;
+  for (int i = 7; i >= 0; i--)
+  {
+    count = notes_count & 0xf;
+    count &&printf("%u %s of Rs %u\n", count, count < 2 ? "note" : "notes", notes[i]);
+    notes_count = notes_count >> 4;
+  }
+  printf("\n");
 }

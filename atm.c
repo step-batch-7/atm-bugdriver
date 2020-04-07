60054c1c965a0b6231891cb073bd8ae8bf47d9a2
rajat <rajat4113@gmail.com>
2020-04-07 15:53:28 +0530

    modified get_money function in atm.c
    now calculating no of notes for each denomination

--- atm.c
+++ atm.c
@@ -3,7 +3,13 @@
 unsigned int get_money(unsigned short int amount)
 {
   unsigned int denomination_count = 0x0;
-  denomination_count |= amount / 2000;
-  denomination_count <<= 28;
+   unsigned short int notes[8] = {2000, 500, 100, 50, 20, 10, 5, 1};
+  unsigned short int remaining_amount = amount;
+  
+  for (int i = 0; i < 8; i++){
+    denomination_count <<=  4;
+    denomination_count |= remaining_amount / notes[i];
+    remaining_amount %=  notes[i];
+  }
   return denomination_count;
 }

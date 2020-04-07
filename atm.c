53bc31b375b115d8f5cf6715113c6ef493940fc8
rajat <rajat4113@gmail.com>
2020-04-07 15:58:11 +0530

    modified get_money function in atm.c
    now returning 0 for values greater than 31999

--- atm.c
+++ atm.c
@@ -2,14 +2,19 @@
 
 unsigned int get_money(unsigned short int amount)
 {
+  if (amount > 31999)
+  {
+    return 0;
+  }
   unsigned int denomination_count = 0x0;
-   unsigned short int notes[8] = {2000, 500, 100, 50, 20, 10, 5, 1};
+  unsigned short int notes[8] = {2000, 500, 100, 50, 20, 10, 5, 1};
   unsigned short int remaining_amount = amount;
-  
-  for (int i = 0; i < 8; i++){
-    denomination_count <<=  4;
+
+  for (int i = 0; i < 8; i++)
+  {
+    denomination_count <<= 4;
     denomination_count |= remaining_amount / notes[i];
-    remaining_amount %=  notes[i];
+    remaining_amount %= notes[i];
   }
   return denomination_count;
 }
